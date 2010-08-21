4d966a09fdaa49c9986c28a365554f285bfeb87b
Christopher Redinger <redinger@gmail.com>
2010-08-20 23:35:53 -0400

    Another params_filters fix

--- lib/hoptoad_notifier/notice.rb
+++ lib/hoptoad_notifier/notice.rb
@@ -319,7 +319,7 @@ module HoptoadNotifier
 
     def also_use_rack_params_filters
       if args[:rack_env]
-        params_filters += rack_request.env["action_dispatch.parameter_filter"] || []
+        params_filters = self.params_filters + (rack_request.env["action_dispatch.parameter_filter"] || [])
       end
     end
 
